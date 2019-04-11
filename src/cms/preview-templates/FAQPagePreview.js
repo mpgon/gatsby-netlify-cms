f989c6bc86440ae6c38ae726ae4a9723c0b3fcf4
MiguelGPereira <miguel.pereira@recaresolutions.com>
2019-04-11 13:44:34 +0200

    Add FAQ

---
+++ src/cms/preview-templates/FAQPagePreview.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { FAQPageTemplate } from "../../templates/faq-page";
+
+export default () => <FAQPageTemplate />;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = class extends React.Component {
     super(props);
     this.state = {
       active: false,
-      navBarActiveClass: ""
+      navBarActiveClass: "",
     };
   }
 
@@ -16,19 +16,19 @@ const Navbar = class extends React.Component {
     // toggle the active boolean in the state
     this.setState(
       {
-        active: !this.state.active
+        active: !this.state.active,
       },
       // after state has been updated,
       () => {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: "is-active"
+              navBarActiveClass: "is-active",
             })
           : this.setState({
-              navBarActiveClass: ""
+              navBarActiveClass: "",
             });
-      }
+      },
     );
   };
 
@@ -60,6 +60,9 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
+              <Link className="navbar-item" to="/faq">
+                FAQ
+              </Link>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
